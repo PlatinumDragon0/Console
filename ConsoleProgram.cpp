f14fb0eaf18ee3d3db0db8da99dd4ea534ffe556
Nathan Destereza <77897742+PlatinumDragon0@users.noreply.github.com>
2024-05-19 18:43:01 +0500

    main

---
+++ ConsoleProgram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+	int a, b;
+
+	std::cout << "Enter first number \n";
+	std::cin >> a;
+
+	std::cout << "Enter second number \n";
+	std::cin >> b;
+
+
+	Power(a,b);
+}
---
+++ Helpers.h
@@ -0,0 +1,5 @@
+int Power(int a, int b)
+{
+	std::cout << a * a << "+" << b * b << "=" << a * a + b * b << "\n";
+	return 0;
+}
